ec1a46847a09eb009f370fdad61e52fa82aff16f
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-01-10 21:55:06 +0530

    Add files via upload

---
+++ Data_types.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a;
+    double b;
+    string str;
+    cin>>a>>b;
+    cin.ignore();
+    getline(cin,str);
+
+    cout<<i+a<<endl;
+    cout<<d+b<<endl;
+    //s=s+str;
+    cout<<s+str<<endl;
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+}
+
---
+++ Hello,World.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    string input_string; 
+    
+    
+    getline(cin, input_string); 
+    
+    // Print a string literal saying "Hello, World." to stdout using cout.
+    cout << "Hello, World." << endl;
+    cout<<input_string<<endl;
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+    return 0;
+}
